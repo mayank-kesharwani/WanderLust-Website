aa64787a7c69ac5004125af7de90356cddccd654
Mayank Kesharwani <harshkesharwaniatarra@gmail.com>
2026-01-13 17:05:34 +0530

    Static Pages Add fixes +2

--- controllers/static.js
+++ controllers/static.js
@@ -1,8 +1,8 @@
 module.exports.privacy = (req, res) => {
   console.log("Rendering privacy page");
-  res.render("./static/privacy");
+  res.render("./static/privacy.ejs");
 };
 
 module.exports.terms = (req, res) => {
-  res.render("static/terms");
+  res.render("static/terms.ejs");
 };
