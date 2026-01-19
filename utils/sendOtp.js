9a3b2db9a95af29bd17019e675324325ac0f58b1
Mayank Kesharwani <harshkesharwaniatarra@gmail.com>
2026-01-19 19:56:27 +0530

    email bug fixes

--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -5,9 +5,28 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 module.exports = async (email, otp) => {
   const msg = {
     to: email,
-    from: process.env.SENDER_EMAIL, // must be verified in SendGrid
-    subject: "WanderLust OTP Verification",
-    text: `Your OTP is ${otp}. It is valid for 5 minutes.`,
+
+    // ✅ ONLY ONE FROM
+    from: {
+      email: process.env.SENDER_EMAIL,
+      name: "WanderLust",
+    },
+
+    subject: "Verify your WanderLust account",
+    text: `Hi 👋,
+
+Welcome to WanderLust!
+
+Your email verification code is:
+
+🔐 ${otp}
+
+This code is valid for 5 minutes.
+If you didn’t request this, you can safely ignore this email.
+
+Thanks,
+WanderLust Team
+https://wanderlust-website-zwnd.onrender.com`,
   };
 
   await sgMail.send(msg);
