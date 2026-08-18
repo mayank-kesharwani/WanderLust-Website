857a55b1d97023b88eefb77b3c38822a345eee98
Mayank Kesharwani <harshkesharwaniatarra@gmail.com>
2026-08-18 12:51:17 +0530

    Brevo Integration

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,100 @@
+// const User = require("../models/user.js");
+// const otpGenerator = require("otp-generator");
+// const sendOtp = require("../utils/sendOtp");
+
+// module.exports.signup = (req, res) => {
+//   res.render("users/signup.ejs");
+// };
+
+// module.exports.signupUser = async (req, res, next) => {
+//   try {
+//     let { username, email, password } = req.body;
+
+//     const existingUser = await User.findOne({ email });
+//     if (existingUser) {
+//       req.flash("error", "Email already registered. Please login.");
+//       return res.redirect("/login");
+//     }
+
+//     const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+//     const otpExpiry = Date.now() + 5 * 60 * 1000;
+
+//     const newUser = new User({
+//       email,
+//       username,
+//       otp,
+//       otpExpiry,
+//       isVerified: false,
+//     });
+//     const registeredUser = await User.register(newUser, password);
+
+//     try {
+//       await sendOtp(email, otp);
+//     } catch (mailErr) {
+//       console.error("OTP email failed:", mailErr);
+
+//       // rollback user creation
+//       await User.findByIdAndDelete(registeredUser._id);
+
+//       req.flash("error", "Unable to send OTP. Please try again.");
+//       return res.redirect("/signup");
+//     }
+
+//     req.flash("success", "OTP sent to your email");
+//     res.render("users/verifyOtp.ejs", { email });
+//   } catch (e) {
+//     req.flash("error", e.message);
+//     res.redirect("/signup");
+//   }
+// };
+
+// module.exports.verifyOtp = async (req, res, next) => {
+//   const { email, otp } = req.body;
+
+//   const user = await User.findOne({ email });
+
+//   if (!user || user.otp !== otp || user.otpExpiry < Date.now()) {
+//     req.flash("error", "Invalid or expired OTP");
+//     return res.redirect("/signup");
+//   }
+
+//   user.isVerified = true;
+//   user.otp = undefined;
+//   user.otpExpiry = undefined;
+//   await user.save();
+
+//   req.login(user, (err) => {
+//     if (err) return next(err);
+
+//     req.flash("success", "Email verified! Welcome to WanderLust");
+//     res.redirect("/listings");
+//   });
+// };
+
+// module.exports.login = (req, res) => {
+//   res.render("users/login.ejs");
+// };
+// module.exports.loginUser = async (req, res) => {
+//   if (!req.user.isVerified) {
+//     req.logout(() => {});
+//     req.flash("error", "Please verify your email first");
+//     return res.redirect("/login");
+//   }
+
+//   req.flash("success", "Welcome back to Wanderlust");
+//   let redirectUrl = res.locals.redirectUrl || "/listings";
+//   res.redirect(redirectUrl);
+// };
+// module.exports.logoutUser = (req, res, next) => {
+//   req.logout((err) => {
+//     if (err) {
+//       next(err);
+//     }
+//     req.flash("success", "You are logged out!");
+//     res.redirect("/listings");
+//   });
+// };
 const User = require("../models/user.js");
 const sendOtp = require("../utils/sendOtp");
 const Listing = require("../models/listing.js");
@@ -114,7 +211,7 @@ module.exports.resendOtp = async (req, res) => {
     return res.status(429).json({ error: `Wait ${wait}s` });
   }
 
-  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  const otp = generateOTP();
 
   user.otp = otp;
   user.otpExpiry = Date.now() + 5 * 60 * 1000;
--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -1,19 +1,60 @@
-const sgMail = require("@sendgrid/mail");
+// const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// module.exports = async (email, otp) => {
+//   const msg = {
+//     to: email,
+
+//     // ✅ ONLY ONE FROM
+//     from: {
+//       email: process.env.SENDER_EMAIL,
+//       name: "WanderLust",
+//     },
+
+//     subject: "Verify your WanderLust account",
+//     text: `Hi 👋,
+
+// Welcome to WanderLust!
+
+// Your email verification code is:
+
+// 🔐 ${otp}
+
+// This code is valid for 5 minutes.
+// If you didn’t request this, you can safely ignore this email.
+
+// Thanks,
+// WanderLust Team
+// https://wanderlust-website-zwnd.onrender.com/listings`,
+//   };
+
+//   await sgMail.send(msg);
+// };
+
+const { BrevoClient } = require("@getbrevo/brevo");
+
+const brevo = new BrevoClient({
+  apiKey: process.env.BREVO_API_KEY,
+});
 
 module.exports = async (email, otp) => {
-  const msg = {
-    to: email,
+  try {
+    const response = await brevo.transactionalEmails.sendTransacEmail({
+      sender: {
+        email: process.env.SENDER_EMAIL,
+        name: "WanderLust",
+      },
 
-    // ✅ ONLY ONE FROM
-    from: {
-      email: process.env.SENDER_EMAIL,
-      name: "WanderLust",
-    },
+      to: [
+        {
+          email: email,
+        },
+      ],
 
-    subject: "Verify your WanderLust account",
-    text: `Hi 👋,
+      subject: "Verify your WanderLust account",
+
+      textContent: `Hi 👋,
 
 Welcome to WanderLust!
 
@@ -22,12 +63,24 @@ Your email verification code is:
 🔐 ${otp}
 
 This code is valid for 5 minutes.
+
 If you didn’t request this, you can safely ignore this email.
 
 Thanks,
 WanderLust Team
-https://wanderlust-website-zwnd.onrender.com/listings`,
-  };
 
-  await sgMail.send(msg);
+https://wanderlust-website-zwnd.onrender.com/listings`,
+    });
+
+    console.log("OTP email sent:", response.messageId);
+
+    return response;
+  } catch (error) {
+    console.error(
+      "Brevo email error:",
+      error?.body || error?.message || error
+    );
+
+    throw error;
+  }
 };
